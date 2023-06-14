b4809510daabac8f6e869c74ffc88eefeb877070
carlos-brazon <carlosbrazon.sp@gmail.com>
2023-06-14 21:26:06 +0200

    arregladas las imagenes del clima

--- js/main.js
+++ js/main.js
@@ -6,15 +6,30 @@ const inicial = async () =>{
     
     form.addEventListener("submit", async function(event) {
         event.preventDefault();
+        
         let value = input.value;
         let urlHora = `https://api.openweathermap.org/data/2.5/weather?q=${value}&lang=sp&appid=c3e85a54f6bc7c74f73c97ed34484a8d`
         let urlDia = `https://api.openweathermap.org/data/2.5/forecast?q=${value}&lang=sp&appid=c3e85a54f6bc7c74f73c97ed34484a8d`
+        let urlDia2 = `https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${value}&lang=sp&appid=c3e85a54f6bc7c74f73c97ed34484a8d`
 
       const infoClima = await getClimaHora(urlHora);
       const infoClimaDia = await getClimaHora(urlDia);
-  
-          await paintInfoWeather(infoClima, infoClimaDia);
-          input.value=''
+
+      if (infoClima.cod==='404') {
+        window.alert('city not found')
+        const div = document.querySelector('.nuevo')
+        const div2 = document.querySelector('.climaDiario')
+        div.innerHTML='';
+        div2.innerHTML='';
+
+      }
+      else{
+        console.log(infoClima);
+        console.log(infoClimaDia);
+    
+            await paintInfoWeather(infoClima, infoClimaDia);
+            input.value=''
+      }
         });   
 }
 inicial();
--- utils/funciones.js
+++ utils/funciones.js
@@ -8,16 +8,17 @@ export const getClimaHora = async (url) => {
 export const paintInfoWeather = async (objeto, diario) =>{
     const div = document.querySelector('.nuevo')
     const div2 = document.querySelector('.climaDiario')
+    const hora = (new Date(objeto.dt*1000)).toLocaleString();
     div.innerHTML=`
             <div class="climaHora">
                 <div class="infoClima">
                     <h1>${objeto.name}</h1>
-                    <p>fecha</p>
+                    <p>${hora}</p>
                     <h2>${Math.round(objeto.main.temp-273.15)} ºC</h2>
                 </div>
                 <div class="lluvia">
                     <div class="logo">
-                        <img src="./assets/${objeto.weather[0].id}.png" alt="">
+                        <img src="https://openweathermap.org/img/wn/${objeto.weather[0].icon}@2x.png" alt="">
                         <p>${objeto.weather[0].description}</p>
                     </div>
                     <p> velocidad del viento: ${(objeto.wind.speed*3.6).toFixed(2)} Km/h </p>
@@ -26,31 +27,31 @@ export const paintInfoWeather = async (objeto, diario) =>{
             `
         div2.innerHTML= `
             <div class="diario">
-                <p>${diario.list[0].dt_txt.slice(0,-9)}</p>
+                <p>${diario.list[0].dt_txt}</p>
                 <div class="info">
-                    <span>${Math.round(diario.list[0].main.temp_min-273.15)}ºC - ${Math.round(diario.list[0].main.temp_max-273.15)}ºC</span>
+                    <span>${Math.round(diario.list[4].main.temp_min-273.15)}ºC - ${Math.round(diario.list[0].main.temp_max-273.15)}ºC</span>
                     <div class="imagen">
-                        <img src="./assets/${diario.list[0].weather[0].id}.png" alt="">
+                        <img src="https://openweathermap.org/img/wn/${diario.list[0].weather[0].icon}@2x.png" alt="aquí va una imagen de una nube">
                         <p>${diario.list[0].weather[0].description}</p>
-                    </div>
-                </div>
-            </div>
-            <div class="diario">
-                <p>${diario.list[8].dt_txt.slice(0,-9)}</p>
-                <div class="info">
-                    <span>${Math.round(diario.list[8].main.temp_min-273.15)}ºC - ${Math.round(diario.list[8].main.temp_max-273.15)}ºC</span>
-                    <div class="imagen">
-                        <img src="./assets/${diario.list[8].weather[0].id}.png" alt="">
+                        </div>
+                        </div>
+                        </div>
+                        <div class="diario">
+                        <p>${diario.list[8].dt_txt}</p>
+                        <div class="info">
+                        <span>${Math.round(diario.list[12].main.temp_min-273.15)}ºC - ${Math.round(diario.list[8].main.temp_max-273.15)}ºC</span>
+                        <div class="imagen">
+                        <img src="https://openweathermap.org/img/wn/${diario.list[8].weather[0].icon}@2x.png" alt="aquí va una imagen de una nube">
                         <p>${diario.list[8].weather[0].description}</p>
-                    </div>
-                </div>
-            </div>
-            <div class="diario">
-                <p>${diario.list[16].dt_txt.slice(0,-9)}</p>
-                <div class="info">
-                    <span>${Math.round(diario.list[16].main.temp_min-273.15)}ºC - ${Math.round(diario.list[16].main.temp_max-273.15)}ºC</span>
-                    <div class="imagen">
-                        <img src="./assets/${diario.list[16].weather[0].id}.png" alt="">
+                        </div>
+                        </div>
+                        </div>
+                        <div class="diario">
+                        <p>${diario.list[16].dt_txt.slice(0,-9)}</p>
+                        <div class="info">
+                        <span>${Math.round(diario.list[20].main.temp_min-273.15)}ºC - ${Math.round(diario.list[16].main.temp_max-273.15)}ºC</span>
+                        <div class="imagen">
+                        <img src="https://openweathermap.org/img/wn/${diario.list[16].weather[0].icon}@2x.png" alt="aquí va una imagen de una nube">
                         <p>${diario.list[16].weather[0].description}</p>
                     </div>
                 </div>
